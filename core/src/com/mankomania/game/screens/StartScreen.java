42e1545724bcf6d8725310a167a13b920c8bf3fc
Markus Kofler <markkofler@edu.aau.at>
2022-04-08 19:14:18 +0200

    #7 Implement Start Screen Drawing Logic

--- core/src/com/mankomania/game/screens/StartScreen.java
+++ core/src/com/mankomania/game/screens/StartScreen.java
@@ -1,2 +1,80 @@
-package com.mankomania.game;public class StartScreen {
+package com.mankomania.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import org.w3c.dom.Text;
+
+public class StartScreen extends ScreenAdapter {
+    private final Stage stage;
+    private final Texture background;
+    private final SpriteBatch batch;
+
+    public StartScreen() {
+        Table table;
+        stage = new Stage();
+        batch = new SpriteBatch();
+        background = new Texture("background.jpg");
+
+        Gdx.input.setInputProcessor(stage);
+
+        initTableAndAddActor(stage);
+    }
+
+    private void initTableAndAddActor(Stage stage) {
+        Skin skin = new Skin(Gdx.files.internal("skin/comic-ui.json"));
+        Texture logoTexture = new Texture(Gdx.files.internal("mankomania.png"));
+
+        skin.getFont("font").getData().setScale(5, 5);
+        Image logoImage = new Image(logoTexture);
+
+        TextButton startButton = new TextButton("START", skin, "default");
+        TextButton settingsButton = new TextButton("SETTINGS", skin, "default");
+        startButton.getLabel().setFontScale(Gdx.graphics.getHeight() / 400f);
+        settingsButton.getLabel().setFontScale(Gdx.graphics.getHeight() / 400f);
+
+        stage.addActor(buildTable(startButton, settingsButton, logoImage));
+    }
+
+    private Table buildTable(TextButton startButton, TextButton settingsButton, Image logoImage) {
+        Table tab = new Table();
+        tab.setFillParent(true);
+        tab.setWidth(stage.getWidth());
+        tab.align(Align.center | Align.top);
+        tab.padTop(Gdx.graphics.getHeight() / 20f);
+        tab.add(logoImage).width(Gdx.graphics.getWidth() / 1.5f).height(Gdx.graphics.getHeight() / 4f);
+        tab.row();
+        tab.add(startButton).padBottom(50).width(Gdx.graphics.getWidth() / 2f).height(Gdx.graphics.getHeight() / 5f);
+        tab.row();
+        tab.add(settingsButton).padBottom(50).width(Gdx.graphics.getWidth() / 2f).height(Gdx.graphics.getHeight() / 5f);
+        tab.row();
+        return tab;
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        ScreenUtils.clear(1, 1, 1, 1);
+        stage.act(delta);
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+        stage.draw();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
 }
