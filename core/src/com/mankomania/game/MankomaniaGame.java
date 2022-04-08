9baf76fd2dabb5120874b47a89b56b72cbcbc953
Markus Kofler <markkofler@edu.aau.at>
2022-04-08 20:07:43 +0200

    #7 Change Initial Screen to Loading Screen and Implement Loading

--- core/src/com/mankomania/game/MankomaniaGame.java
+++ core/src/com/mankomania/game/MankomaniaGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mankomania.game.screens.LoadingScreen;
 import com.mankomania.game.screens.StartScreen;
 
 public class MankomaniaGame extends Game {
@@ -16,7 +17,7 @@ public class MankomaniaGame extends Game {
 		com.badlogic.gdx.Screen currentScreen = this.getScreen();
 
 		//Set new Screen
-		ScreenAdapter newScreen = new StartScreen();
+		ScreenAdapter newScreen = new LoadingScreen();
 		this.setScreen(newScreen);
 
 		//dispose old Screen
