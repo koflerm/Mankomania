a9577f461e819a214f9a2a9f4759afec81092308
Markus Kofler <markkofler@edu.aau.at>
2022-04-09 16:30:50 +0200

    #10 Cleanup unused dependencies

--- core/src/com/mankomania/game/screens/LoadingScreen.java
+++ core/src/com/mankomania/game/screens/LoadingScreen.java
@@ -6,19 +6,9 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.mankomania.game.MankomaniaGame;
-
-import sun.awt.image.GifImageDecoder;
 
 public class LoadingScreen extends ScreenAdapter {
     private Stage stage;
