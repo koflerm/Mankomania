5bbdbe2c7e49d74670104e64cf99044dcb343446
christofplie <97967480+christofplie@users.noreply.github.com>
2022-04-10 11:43:24 +0200

    Fix Rebase Issues

--- core/src/com/mankomania/game/screens/StartScreen.java
+++ core/src/com/mankomania/game/screens/StartScreen.java
@@ -13,7 +13,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.mankomania.game.MankomaniaGame;
 
 public class StartScreen extends ScreenAdapter {
