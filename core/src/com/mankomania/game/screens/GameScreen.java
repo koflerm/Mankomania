b03f76e07d814225484e7591202aa332706cacf6
Markus Kofler <markkofler@edu.aau.at>
2022-04-19 20:14:13 +0200

    #10 Add TurnDialog Modal

--- core/src/com/mankomania/game/screens/GameScreen.java
+++ core/src/com/mankomania/game/screens/GameScreen.java
@@ -1,6 +1,7 @@
 package com.mankomania.game.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
@@ -9,8 +10,12 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -18,10 +23,16 @@ public class GameScreen extends ScreenAdapter {
     private final Texture gameBoard;
     private final Stage stage;
     private final SpriteBatch batch;
-    //private final Texture boxTexture;
     private final Skin skin;
     private final float boxWidth;
     private final float boxHeight;
+    private Dialog turnDialog;
+
+    private boolean turnDialogIsShown;
+    private boolean turnDialogNeeded;
+    private String turnDialogPlayerName;
+    private String turnDialogPlayerMoney;
+    private boolean turnDialogIsCurrentPlayer;
 
     private static final float BOARD_SIZING_FACTOR = 1f;
     private static final float BOARD_PLAYER_BOX_WIDTH_FACTOR = 4.5f;
@@ -36,16 +47,40 @@ public class GameScreen extends ScreenAdapter {
         skin = new Skin(Gdx.files.internal("skin/comic-ui.json"));
         boxWidth = calcWidthFactor(BOARD_PLAYER_BOX_WIDTH_FACTOR);
         boxHeight = calcHeightFactor(BOARD_PLAYER_BOX_HEIGHT_FACTOR);
+        turnDialogIsShown = false;
+        turnDialog = new Dialog("INFO", skin, "alt") {};
+        turnDialogNeeded = false;
+
+        InputMultiplexer inputMultiplexer = (InputMultiplexer) Gdx.input.getInputProcessor();
+        if (!inputMultiplexer.getProcessors().contains(stage, true)) {
+            inputMultiplexer.addProcessor(stage);
+        }
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        ScreenUtils.clear(0.9f, 0.9f, 0.9f, 1);
         stage.act(delta);
+        ScreenUtils.clear(0.9f, 0.9f, 0.9f, 1);
         drawGameBoard();
         drawPlayerInformation();
+        if (!turnDialogIsShown && turnDialogNeeded) {
+            drawTurnDialog();
+            turnDialogIsShown = true;
+        }
         stage.draw();
+    }
+
+    public void showTurnDialog(String playerName, String playerMoney, boolean isCurrentPlayer) {
+        this.turnDialogNeeded = true;
+        this.turnDialogPlayerName = playerName;
+        this.turnDialogPlayerMoney = playerMoney;
+        this.turnDialogIsCurrentPlayer = isCurrentPlayer;
+    }
+
+    public void hideTurnDialog() {
+        this.turnDialogNeeded = false;
+        this.turnDialog.hide();
     }
 
     private void drawGameBoard() {
@@ -66,7 +101,7 @@ public class GameScreen extends ScreenAdapter {
         drawPlayerBox(0, Gdx.graphics.getHeight() - boxHeight, "P2", Color.GOLDENROD);
         drawPlayerBox(Gdx.graphics.getWidth() - boxWidth, Gdx.graphics.getHeight() - boxHeight, "P3", Color.BLUE);
         drawPlayerBox(Gdx.graphics.getWidth() - boxWidth, 0, "P4", Color.RED);
-        drawPlayerMetadata(Gdx.graphics.getWidth() / 2 - (boxWidth / 2), 100000);
+        //drawPlayerMetadata(Gdx.graphics.getWidth() / 2 - (boxWidth / 2), 100000);
         batch.end();
     }
 
@@ -92,6 +127,35 @@ public class GameScreen extends ScreenAdapter {
         label.setPosition(0, 0);
         label.setAlignment(Align.center);
         stage.addActor(label);
+    }
+
+    private void drawTurnDialog() {
+        float scale = Gdx.graphics.getWidth() / 1000f;
+
+        Table tab = new Table();
+        tab.align(Align.center);
+        Label l1 = new Label("Next Turn:", skin, "title");
+        Label l2 = new Label(turnDialogPlayerName, skin, "title");
+        Label l3 = new Label("Current money: $" + turnDialogPlayerMoney, skin, "title");
+
+        tab.add(l1).row();
+        tab.add(l2).row();
+        tab.add(l3).row();
+        tab.pad(20);
+
+        if (turnDialogIsCurrentPlayer) {
+            TextButton b1 = new TextButton("START TURN", skin, "default");
+            b1.setTransform(true);
+            turnDialog.button(b1, true).padBottom(20);
+        } else {
+            Label l4 = new Label("Waiting for player...", skin, "default");
+            tab.add(l4).row();
+        }
+
+        turnDialog.getContentTable().add(tab);
+        turnDialog.setScale(scale);
+        turnDialog.show(stage);
+        turnDialog.setPosition(Gdx.graphics.getWidth() / 2 - ((turnDialog.getWidth() * scale) / 2), Gdx.graphics.getHeight() / 2 - ((turnDialog.getHeight() * scale) / 2));
     }
 
     private float calcBoardPosition(int base) {
