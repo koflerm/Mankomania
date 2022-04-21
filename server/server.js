4da0e782eaf332b4e49610cdd2130fa63d1b99fe
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2022-04-21 13:52:14 +0200

    Added first custom-event

--- server/server.js
+++ server/server.js
@@ -1,2 +1,16 @@
-const express = require('express');
-const app = express();
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = 3000;
+
+server.listen(port, function(){
+    console.log("Server running at " + port);
+})
+
+io.on("connection",socket => {
+    console.log(socket.id)
+    socket.on("custom-event", (number, string, obj) =>{
+        console.log(number, string, obj)
+    })
+})
+
