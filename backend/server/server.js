5a03f32719f94f445ae4732aac0963a63d492229
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2022-04-22 10:10:25 +0200

    Finished client1 for testing

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,19 +1,35 @@
 import express from 'express';
 import { Server } from 'socket.io';
+import { instrument } from "@socket.io/admin-ui"
 const app = express();
 const server = app.listen(3000, function(){
     console.log("Server running at " + 3000);
 })
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, { cors: { origin: '*'} });
 
 
 
 
 io.on("connection",socket => {
     console.log(socket.id)
-    socket.on("custom-event", (number, string, obj) =>{
-        console.log(number, string, obj)
+    socket.on("send-message", (message, room) => {
+        if(room === ""){
+            socket.broadcast.emit('receive-message', message)
+        }else{
+            socket.to(room).emit('receive-message', message)
+        }
+
+
+    })
+
+    socket.on('join-room', (room, cb) =>{
+        socket.join(room)
+        cb(`Joined ${room}`)
     })
 })
+
+instrument(io, {
+    auth: false
+});
 
 
