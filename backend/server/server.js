e55b12761894e3721206a3dc8f58d3c3629ec562
Christof Plieschnegger <97967480+christofplie@users.noreply.github.com>
2022-05-01 11:12:31 +0200

    Revert "#12 Implemented first lobby setup"
    This reverts commit 293df9ff70ba4df7b9be0fcfaa67d90e09690961.

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,64 +1,15 @@
-const express = require('express')
+import express from 'express';
+import { Server } from 'socket.io';
+import { instrument } from "@socket.io/admin-ui"
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const {Server} = require("socket.io");
+const server = app.listen(3000, function(){
+    console.log("Server running at " + 3000);
+})
 const io = new Server(server, { cors: { origin: '*'} });
-const PORT = process.env.PORT || 3000;
-//const { instrument } = require("@socket.io/admin-ui");
-
-let clientNo = 1;
-
-app.get('/', (req, res) =>{
-    res.write(`<h1>Socket IO Start on Port : ${PORT}</h1>`)
-    res.end();
-})
-
-server.listen(PORT, ()=>{
-console.log('Listing on*:3000')
-
-    io.on('connection', (socket) => {
-        console.log(socket.id)
-
-        socket.on("join-room", (room) =>{
-            validateRoom(room, socket);
-        });
-
-        socket.on('disconnect', () => {
-            console.log('A user has disconnected.');
-
-        })
-    });
 
 
-})
 
-function validateRoom(room, socket){
-    //toDo with Map
-    if(room === ''){
-        clientNo++;
-        socket.join(Math.round(clientNo/4));
-        socket.emit("join-room", Math.round(clientNo/4))
-        console.log(socket.id + " joined " + Math.round(clientNo/4))
 
-    }else{
-        //client --> check if room is naN
-       socket.join(room);
-       socket.emit('join-room', room);
-       console.log(socket.id + " joined " + room)
-    }
-
-}
-
-function startGame(){
-
-}
-/*
-instrument(io, {
-    auth: false
-});
-*/
-/*
 io.on("connection",socket => {
     console.log(socket.id)
     socket.on("send-message", (message, room) => {
@@ -67,25 +18,18 @@ io.on("connection",socket => {
         }else{
             socket.to(room).emit('receive-message', message)
         }
+
+
     })
 
     socket.on('join-room', (room, cb) =>{
         socket.join(room)
         cb(`Joined ${room}`)
     })
-
-    socket.on("disconnect",function(){
-        console.log(socket.id + "Disconnected")
-
-    });
-
 })
 
- */
-/*
-io.on('connection', () =>{
-    socket.on('message', msg =>{
-        io.emit('message', msg)
-    })
-})
-*/
+instrument(io, {
+    auth: false
+});
+
+
