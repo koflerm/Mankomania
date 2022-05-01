ee57e63f02fec22de1c646e24045ef17f2c150f2
sinardone@edu.aau.at <sinardone@edu.aau.at>
2022-05-01 20:15:25 +0200

    #40 add and implement class StockExchange

---
+++ core/src/boardLogic/minigames/stockExchangeLogic/StockExchange.java
@@ -0,0 +1,38 @@
+package boardLogic.minigames.stockExchangeLogic;
+
+import shareLogic.Share;
+
+public class StockExchange {
+    private Share share;
+    private boolean isRising;
+    private boolean needUpdate;
+
+    public boolean isRising() {
+        return isRising;
+    }
+
+    public boolean needUpdate() {
+        return needUpdate;
+    }
+
+    //----------GETTERS------------
+    public Share getShare() {
+        return share;
+    }
+
+    //-----------------------------
+    //----------SETTERS------------
+    public void setRising(boolean rising) {
+        isRising = rising;
+    }
+
+    public void setShare(Share share) {
+        this.share = share;
+    }
+
+    public void setNeedUpdate(boolean needUpdate) {
+        this.needUpdate = needUpdate;
+    }
+//-----------------------------
+//TODO add roulette method to check if the stock are rising or not (random logic)
+}
