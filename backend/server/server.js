f733fb1d090d73b1dafc1da9083a29e57d11ec2a
Christof Plieschnegger <97967480+christofplie@users.noreply.github.com>
2022-05-01 11:12:31 +0200

    Revert "Finished client1 for testing"
    This reverts commit 4bd477ae9a64d0d1ff4b1283e4064f974ff3ab88.

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,35 +1,19 @@
 import express from 'express';
 import { Server } from 'socket.io';
-import { instrument } from "@socket.io/admin-ui"
 const app = express();
 const server = app.listen(3000, function(){
     console.log("Server running at " + 3000);
 })
-const io = new Server(server, { cors: { origin: '*'} });
+const io = new Server(server, { cors: { origin: '*' } });
 
 
 
 
 io.on("connection",socket => {
     console.log(socket.id)
-    socket.on("send-message", (message, room) => {
-        if(room === ""){
-            socket.broadcast.emit('receive-message', message)
-        }else{
-            socket.to(room).emit('receive-message', message)
-        }
-
-
-    })
-
-    socket.on('join-room', (room, cb) =>{
-        socket.join(room)
-        cb(`Joined ${room}`)
+    socket.on("custom-event", (number, string, obj) =>{
+        console.log(number, string, obj)
     })
 })
-
-instrument(io, {
-    auth: false
-});
 
 
