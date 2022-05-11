2ffa1be3beef025f72ba29946c2cf21e745d032d
sinardone@edu.aau.at <sinardone@edu.aau.at>
2022-05-11 17:42:46 +0200

    #40 delete example of spin the wheel

--- core/src/boardLogic/minigames/stockExchangeLogic/StockExchange.java
+++ core/src/boardLogic/minigames/stockExchangeLogic/StockExchange.java
@@ -37,29 +37,5 @@ public class StockExchange {
         this.needUpdate = needUpdate;
     }
 //-----------------------------
-    public void spinTheWheel(){
-        int shareID = secRand.nextInt(shareTypeAmount - 1) + 1;
-
-        switch (shareID){
-            case 1: setShare(Share.SHORT_CIRCUIT_PLC);
-                    setRising(true);
-                    break;
-            case 2: setShare(Share.SHORT_CIRCUIT_PLC);
-                    setRising(false);
-                    break;
-            case 3: setShare(Share.HARD_STEEL_PLC);
-                    setRising(true);
-                    break;
-            case 4: setShare(Share.HARD_STEEL_PLC);
-                    setRising(false);
-                    break;
-            case 5: setShare(Share.DRY_OIL_PLC);
-                    setRising(true);
-                    break;
-            case 6: setShare(Share.DRY_OIL_PLC);
-                    setRising(false);
-                    break;
-        }
-    }
 
 }
