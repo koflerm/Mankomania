58cb8f492998c58ac1a6846852d332b96c6365ed
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2022-05-27 11:29:22 +0200

    #16 Added documentation

--- backend/server/server.js
+++ backend/server/server.js
@@ -6,7 +6,6 @@ const {Server} = require("socket.io");
 const io = new Server(server, { cors: { origin: '*'} });
 const PORT = process.env.PORT || 3000;
 const { v4: uuidv4 } = require('uuid');
-const Json = require("nodemon");
 //const { instrument } = require("@socket.io/admin-ui");
 
 
@@ -24,16 +23,15 @@ const MAX_LOBBY_SIZE = 4
  * Check if a private Room was send or a new room should be created or join to an room
  * @param socket A connected socket.io socket
  * @param room Name of the room
- * @param io server instance
  */
-function validateRoom(socket, room, io){
+function validateRoom(socket, room){
     if(room === ''){
         let roomID = searchEmptyRooms();
         if (roomID === undefined){
-            joinRoom(socket, createRoom(), io);
+            joinRoom(socket, createRoom());
 
         }else{
-            joinRoom(socket,roomID, io)
+            joinRoom(socket,roomID)
         }
 
     }else{
@@ -85,24 +83,34 @@ function searchEmptyRooms(){
  * Will connect an socket to an specified room
  * @param socket A connected socket.io socket
  * @param room An object that represents a room from the `rooms` instance variable object
- * @param io server instance
  */
-function joinRoom(socket, room, io) {
+function joinRoom(socket, room) {
+    //push socket id into socket array in room object
     room.sockets.push(socket.id);
+
     createPlayer(room, socket.id, stocks())
+    //socket joins room
     socket.join(room.id);
     console.log(socket.id, "Joined", room.id);
     io.in(room.id).emit('JOIN_ROOM', room.id, room.sockets)
 
+    //when lobby is full
     if(room.sockets.length === MAX_LOBBY_SIZE){
         room.status = true;
-        //console.log(room.id + " is full")
         io.in(room.id).emit('START_GAME', room.id, room);
         console.log(rooms[room.id])
+        //create a new Room for other players
         createRoom();
 
     }
 }
+
+/**
+ * Will create a new player object and insert it into the players object
+ * @param room An object that represents a room from the `rooms` instance variable object
+ * @param socket An connected socket.io socket
+ * @param stocks An object that represents stocks and their default values
+ */
 const createPlayer = (room,socket, stocks) =>{
    const player = {
         socket: socket,
@@ -120,11 +128,13 @@ const createPlayer = (room,socket, stocks) =>{
            this.dice_Count = this.dice_1 +  this.dice_2
        }
     }
+    //insert into players object
     rooms[room.id].players[socket] = player
-
 }
-
-
+/**
+ * Will return an object with stocks and default value
+ * @returns {{ShortCircuit_PLC: number, HardSteel_PLC: number, DryOil_PLC: number}}
+ */
 const stocks = ()=> {
     return  {
         HardSteel_PLC: 0,
@@ -146,7 +156,6 @@ function leaveRooms(socket, io){
 
         if(room.sockets.includes(socket.id)){
                 socket.leave(id)
-
             // remove the socket from the room object
             room.sockets = room.sockets.filter((item) => item !== socket.id);
             console.log(room.sockets)
@@ -182,56 +191,79 @@ function increaseReadyCounterForRoom(socket, room, io){
         io.to(socket).emit('ERROR', "Couldn't find Room");
     }
 }
+
+/**
+ * Will save dice1,dice2 and calculate the total dice count
+ * @param room  An object that represents a room from the `rooms` instance variable object
+ * @param socket A connected socket.io socket
+ * @param diceCount A counter to check how many clients have rolled their dice
+ * @param winnerLength The length of the winner array
+ */
 const saveDice = (room, socket, diceCount, winnerLength) =>{
         rooms[room].players[socket.id].calculateDiceCount(diceCount)
         rooms[room].counterForDice++
 
-
         if (winnerLength != null){//if we have one or more winner
             if (rooms[room].counterForDice === winnerLength){
-                console.log(rooms[room].players[socket.id])
+                //console.log(rooms[room].players[socket.id])
                 validateHighestDice(room)
             }
         }else{
+            //if all clients have rolled their dice
             if (rooms[room].counterForDice === rooms[room].sockets.length){
                 validateHighestDice(room)
             }
         }
 }
-
+/**
+ * Will validate the highest Dice count and send the winner/s
+ * @param room An object that represents a room from the `rooms` instance variable object
+ */
 const validateHighestDice = (room) =>{
+    //convert players object into an array
     let data = Object.values(rooms[room].players)
+
+    //returns the highest dice count of all players
     let highestDice = Math.max.apply(Math, data.map(function(o) {
         return o.dice_Count;
     }))
 
-
-    console.log("Highest Dice: " + highestDice)
-
+    //returns the winner player/s
     let winner = data.filter(x => [x.dice_Count] == highestDice)
     console.log(winner)
+    //if we have one winner
     if(winner.length === 1){
-        //if we have one winner
         io.in(room).emit('START_ROUND', data, winner[0].socket);
         resetPlayersDice(room)
-    }else{
-        //if we have to ore more winners
+    }
+    //if we have to ore more winners
+    else{
         io.in(room).emit('ROLE_THE_HIGHEST_DICE_AGAIN', data, winner);
         resetPlayersDice(room)
     }
 }
-
+/**
+ * Will reset dice_1, dice_2, dice_Count and counterForDice
+ * @param room An object that represents a room from the `rooms` instance variable object
+ */
 const resetPlayersDice = (room) =>{
-    Object.values(rooms[room].players).map(a=>a.dice_1 = 0);
+    Object.values(rooms[room].players).map(a=>a.dice_1= 0);
     Object.values(rooms[room].players).map(a=>a.dice_2 = 0);
     Object.values(rooms[room].players).map(a=>a. dice_Count = 0);
     rooms[room].counterForDice = 0
     console.log(rooms[room])
 }
 
-
+/**
+ * Will update the player object with the new stock object
+ * @param room An object that represents a room from the `rooms` instance variable object
+ * @param socket An connected socket.io socket
+ * @param stock An object that represents stocks and their default values
+ */
 const updateStock = (room, socket, stock)=>{
+    //increase the stockCounter
     rooms[room].stockCounterFunction(room, socket, stock)
+    //check if all clients have selected their stocks
     if (rooms[room].counterForStocks === rooms[room].sockets.length){
         console.log("Update Stock finished " + rooms[room].sockets)
         io.in(room).emit('ROLE_THE_HIGHEST_DICE');
@@ -255,7 +287,6 @@ io.on('connection', (socket) => {
     console.log(rooms)
 
     socket.on('ROLE_THE_HIGHEST_DICE', (room, diceCount) =>{
-        //console.log(diceCount)
         saveDice(room, socket, diceCount)
     })
 
