079968eee90ab8c2318a522d0c0749f32e7ed2d0
RaphDef <62890716+RaphDef@users.noreply.github.com>
2022-06-01 22:39:55 +0200

    Refactoring

--- core/src/com/mankomania/game/Connection.java
+++ core/src/com/mankomania/game/Connection.java
@@ -1,9 +1,15 @@
 package com.mankomania.game;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import playerLogic.Player;
 
 public class Connection {
     private static final String SERVER = "https://mankomania-backend.herokuapp.com/";
@@ -13,7 +19,7 @@ public class Connection {
     private static String lobbyID;
     private static String[] players = {"", "", "", ""};
 
-    public static void createConnection(){
+    public static void createConnection() {
         try {
             cs = IO.socket(SERVER);
             cs.connect();
@@ -24,8 +30,8 @@ public class Connection {
         }
     }
 
-    private Connection(){
-        
+    private Connection() {
+
     }
 
     public static Socket getCs() {
@@ -61,9 +67,6 @@ public class Connection {
     }
 
 
-
-
-
     public static void joinRoom(Emitter.Listener el) {
         cs.on("JOIN_ROOM", el);
         cs.emit("JOIN_ROOM", "");
@@ -82,13 +85,13 @@ public class Connection {
 
     /**
      * new Methods
-     * **/
+     **/
 
     public static void emitStocks() {
         cs.emit("CHOSE_STOCKS", lobbyID, "{ HardSteel_PLC: 2, ShortCircuit_PLC: 0, DryOil_PLC: 0 }");
     }
 
-    public static void roleHighestDice (Emitter.Listener el) {
+    public static void roleHighestDice(Emitter.Listener el) {
         cs.once("ROLE_THE_HIGHEST_DICE", el);
     }
 
@@ -110,12 +113,146 @@ public class Connection {
     public static void startRound(Emitter.Listener el) {
         cs.on("START_ROUND", el);
     }
-  /* Bekommen: Gewinner
-  {const dice = {
-          socket: socket.id,
-          dice: diceCount
-          }
-   }
-   */
 
+    public static void convertJsonToPlayer(String args) {
+
+        /**
+         * Split Player-Array in multiple Player Strings
+         */
+
+        String[] players = args.split("},", 10);
+
+        for (int i = 0; i < players.length; i++) {
+
+            String temp = players[i];
+
+            String newString = "";
+
+            if (i == 0) {
+                //Ersten 24 Zeichen entfernen
+                //} hinten hinzufügen
+
+                newString = temp.substring(24);
+                newString += "}";
+                players[i] = newString;
+            } else if (i < (players.length - 1)) {
+                //Ersten 23 Zeichen entfernen
+                //} hinten hinzufügen
+
+                newString = temp.substring(23);
+                newString += "}";
+
+                players[i] = newString;
+
+            } else if (i == (players.length - 1)) {
+                //Ersten 23 Zeichen entfernen
+                //Hinten entfernen: }
+
+                newString = temp.substring(23);
+                newString = newString.substring(0, newString.length() - 1);
+                players[i] = newString;
+            }
+
+        }
+
+        /**
+         * Parser for Stocks
+         */
+
+        ArrayList<String> stockAsString = new ArrayList<String>();
+
+
+        for (int i = 0; i < players.length; i++) {
+            String[] stock = players[i].split(":\\{");
+
+            String newStock = stock[1];
+
+            String newNewStock = "{";
+
+            newNewStock += newStock;
+
+            String finalStock = newNewStock.substring(0, newNewStock.length() - 1);
+
+            stockAsString.add(finalStock);
+        }
+
+
+        /**
+         * Parse Player-Strings into Con-Players
+         */
+
+        ArrayList<String> allPlayersAsString = new ArrayList<String>();
+
+        for(String s : players){
+            allPlayersAsString.add(s);
+        }
+
+        ArrayList<ConPlayer> cp = new ArrayList<ConPlayer>();
+
+        for (String p : allPlayersAsString) {
+
+            JsonObject jsonObject = JsonParser.parseString(p).getAsJsonObject();
+
+            ConPlayer temp = new ConPlayer();
+
+            temp.setDice_2(jsonObject.get("dice_2").getAsInt());
+
+            temp.setDice_1(jsonObject.get("dice_1").getAsInt());
+
+            temp.setMoney(jsonObject.get("money").getAsInt());
+
+            temp.setYouTurn(jsonObject.get("yourTurn").getAsBoolean());
+
+            temp.setSocket(jsonObject.get("socket").getAsString());
+
+            temp.setPosition(jsonObject.get("position").getAsInt());
+
+            temp.setDice_Count(jsonObject.get("dice_Count").getAsInt());
+
+            temp.setPlayerIndex(jsonObject.get("playerIndex").getAsInt());
+
+            cp.add(temp);
+
+        }
+
+        /**
+         * Parse Stocks into ConPlayer Object
+         */
+
+        for (int i = 0; i < stockAsString.size(); i++) {
+
+            JsonObject jsonStock = JsonParser.parseString(stockAsString.get(i)).getAsJsonObject();
+
+            cp.get(i).setHARD_STEEL_PLC(jsonStock.get("HardSteel_PLC").getAsInt());
+            cp.get(i).setSHORT_CIRCUIT_PLC(jsonStock.get("ShortCircuit_PLC").getAsInt());
+            cp.get(i).setDRY_OIL_PLC(jsonStock.get("DryOil_PLC").getAsInt());
+        }
+
+
+        /**
+         * Create real Player-Objects
+         */
+
+        for (ConPlayer c : cp) {
+
+            Player p = new Player();
+
+            p.setPlayerIndex(c.getPlayerIndex());
+            p.setPlayerSocketID(c.getSocket());
+            p.setMoney(c.getMoney());
+
+            //Field
+            p.setCurrentFieldPosition(MankomaniaGame.getInstance().getBoard().getFieldByIndex(c.getPosition()));
+
+            //Stocks
+            p.setShares(c.getHARD_STEEL_PLC(), c.getSHORT_CIRCUIT_PLC(), c.getDRY_OIL_PLC());
+
+
+            //Problem
+            //MankomaniaGame.getInstance().getBoard().addPlayer(p);
+
+            System.out.println(p.toString());
+
+        }
+    }
 }
