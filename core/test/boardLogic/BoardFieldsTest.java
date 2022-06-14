eb5f0a18c3c2cc8a7bac75e6ae0a4dfdfc326024
Markus Kofler <markkofler@edu.aau.at>
2022-06-14 21:01:19 +0200

    #76 Add JUnit and Mockito + First Tests

---
+++ core/test/boardLogic/BoardFieldsTest.java
@@ -0,0 +1,30 @@
+package boardLogic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fieldLogic.Field;
+
+public class BoardFieldsTest {
+    private Field[] fields;
+
+    @Before
+    public void initBoardFields() {
+        fields = BoardFields.getFields(20, 20);
+    }
+
+    @Test
+    public void testGetFields() {
+        assertEquals(69, fields.length);
+    }
+
+    @Test
+    public void testConnectFields() {
+        BoardFields.connectFields(fields);
+        for (Field field: fields) {
+            assertNotEquals(null, field.getNextField());
+        }
+    }
+}
