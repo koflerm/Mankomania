bc85eb75669d972edd769375d04bf26a75b7b07d
Markus Kofler <markkofler@edu.aau.at>
2022-06-21 20:43:35 +0200

    #76 Add Additional Unit Tests for Dice and Share

---
+++ core/test/diceLogic/DiceTest.java
@@ -0,0 +1,43 @@
+package diceLogic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class DiceTest {
+
+    private Dice dice;
+
+    @Before
+    public void init() {
+        dice = new Dice();
+    }
+
+    @Test
+    public void throwDiceTest() {
+        List<Integer> dices = dice.throwDice(6, 2);
+        for (Integer dice: dices) {
+            assertEquals(true, dice <= 6 && dice >= 1);
+        }
+    }
+
+    @Test
+    public void getSumOfDiceTest() {
+        List<Integer> dices = dice.throwDice(6, 2);
+        int sum = 0;
+        for (Integer dice: dices) {
+            sum += dice;
+        }
+        int methodSum = dice.getSumOfDice(dices);
+        assertEquals(sum, methodSum);
+    }
+
+    @Test
+    public void getDiceTest() {
+        List<Integer> dices = dice.throwDice(6, 2);
+        assertEquals(dices, dice.getDicee());
+    }
+}
