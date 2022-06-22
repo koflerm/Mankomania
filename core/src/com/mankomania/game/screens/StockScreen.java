5e573fbbb545ee44e8bd08f363a7b8cf50c251a5
christofplie <97967480+christofplie@users.noreply.github.com>
2022-06-22 22:25:16 +0200

    Remove unused Import

--- core/src/com/mankomania/game/screens/StockScreen.java
+++ core/src/com/mankomania/game/screens/StockScreen.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.RotateToAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.ScreenUtils;
