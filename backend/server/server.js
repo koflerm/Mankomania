23c27876b34628d0eca6c1bca800c2377efa81d0
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2022-06-24 12:43:28 +0200

    added link

--- backend/server/server.js
+++ backend/server/server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 const { v4: uuidv4 } = require('uuid');
 const {instrument} = require("@socket.io/admin-ui");
 //sonar-scanner.bat -D"sonar.organization=koflerm" -D"sonar.projectKey=Mankomania_backend" -D"sonar.sources=." -D"sonar.host.url=https://sonarcloud.io"
+//https://sonarcloud.io/summary/new_code?id=Mankomania_backend
 
 
 /**
